9f34834cbce8b3693ff0c5da5aefa80c2ffa6cdc
a-tagiev <antn.tgv@gmail.com>
2023-03-02 13:27:57 +0300

    .

--- fib,lesson 1/last digit.py
+++ fib,lesson 1/last digit.py
@@ -1,13 +1,14 @@
 def fib(n):
-    if n==0:
+    if n == 0:
         return 0
-    arr =[0]*(n+1)
+    arr = [0] * (n + 1)
     arr[0] = 0
-    arr[1]=1
-    if n>=2:
-        for i in range(2, n+1):
-            arr[i] = (arr[i - 1] + arr[i - 2])%10
+    arr[1] = 1
+    if n >= 2:
+        for i in range(2, n + 1):
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10
     return arr[n]
+
 
 def main():
     n = int(input())
